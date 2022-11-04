4e90553a2e51f3786b599aac224f86eadbddbfb9
Roman <roman_stalmakov@mail.ru>
2022-11-04 17:28:28 +0300

    0.3. BufferedReader. [#252489 #357728]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,11 +2,8 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class LogFilter {
     public List<String> filter(String file) {

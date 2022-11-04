67f6c22f206d12b6bfb6724fc638b7dfb3367cf7
Roman <roman_stalmakov@mail.ru>
2022-11-04 15:32:04 +0300

    0.2. FileInputStream [#4898 #357702]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -15,7 +16,7 @@ public class EvenNumberFile {
             for (String line : lines) {
                 System.out.println(Integer.parseInt(line) % 2 == 0);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 

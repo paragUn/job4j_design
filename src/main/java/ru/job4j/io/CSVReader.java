af6fe3c84f672c836ee34343bf49e0ae7abb4d7f
Roman <roman_stalmakov@mail.ru>
2022-12-01 20:18:57 +0300

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -2,12 +2,14 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        checkArgs(argsName);
        List<String> readLines = readCSV(argsName.get("path"));
         String changedLine = lineChanger(readLines,
                 argsName.get("delimiter"),
@@ -31,25 +33,38 @@ public class CSVReader {
 //                "William;30;Brown;Secondary special"
     private static String lineChanger(List<String> readLine, String delimiter, String filter) {
         StringJoiner str = new StringJoiner(delimiter); // выдача результата
-        int[] indexesOfColumn = new int[filter.split(delimiter).length]; // массив индексов
-        List<String> keywordFilter = Arrays.asList(filter.split(delimiter)); //массив ключевых слов
+        List<Integer> intList = new ArrayList<>(); // массив индексов
+
+        List<String> keywordFilter = Arrays.asList(filter.split(",")); //массив ключевых слов
+//        System.out.println("keywordFilter: " +  keywordFilter);
+
         List<String>  columns = Arrays.asList(readLine.get(0).split(delimiter));
-        for (String current : keywordFilter) {
-            if (columns.contains(keywordFilter)) {
-                int i = 0;
-                indexesOfColumn[i] = columns.indexOf(keywordFilter);
-                i++;
-            }
-        }
-        System.out.println(indexesOfColumn);
+//        System.out.println("columns: " +  columns);
+
+//        for (String s : keywordFilter) {
+//            intList.add(columns.indexOf(s));
+//        }
+        keywordFilter.forEach(s -> intList.add(columns.indexOf(s)));
+//        System.out.println("columns[0] = " + columns.get(0));
+//        System.out.println("keywordFilter[0] = " + keywordFilter.get(0));
+
+//        for (Integer i : intList) {
+//            System.out.println("index = " + i);
+//        }
+
+//        readLine.forEach(System.out::println);
+
+//        List<String> splittedReadline =
         for (String current : readLine) {
-            for (int index : indexesOfColumn) {
-                str.add(current.split(delimiter)[index]);
+            for (int index : intList) {
+                String[] splittedReadline = current.split(delimiter);
+                str.add(splittedReadline[index]);
             }
             str.add(System.lineSeparator());
         }
         return str.toString();
     }
+
     private static void saveCSV(String outData, String outPath) {
         if (outPath.equals("stdout")) {
             System.out.println(outData);
@@ -60,6 +75,16 @@ public class CSVReader {
             pw.println(outData);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void checkArgs(ArgsName args) {
+        Path source = Paths.get(args.get("path"));
+        if (!source.toFile().exists() || !source.toFile().isFile()) {
+            throw new IllegalArgumentException("Path not valid, or not exist.");
+        }
+        if (args.get("delimiter") == null) {
+            throw new IllegalArgumentException("Delimiter must be defined.");
         }
     }
 }

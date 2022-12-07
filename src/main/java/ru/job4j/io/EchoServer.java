026ea3fc20cc529bf886b4afa3d043cb78c9b5af
Roman <roman_stalmakov@mail.ru>
2022-12-07 16:36:24 +0300

    7. Scanner [#504791 #368503]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,8 +13,9 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
+                    String str = in.readLine();
+                    System.out.println(str);
+                    if (str != null && !str.isEmpty()) {
                         if (str.contains("/?msg=Bye")) {
                             server.close();
                         }

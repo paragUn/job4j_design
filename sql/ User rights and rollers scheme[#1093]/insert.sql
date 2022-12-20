1d1d67c0bfc75497ac5d47bbc512af275d07e40c
Roman <roman_stalmakov@mail.ru>
2022-12-20 11:43:35 +0300

    0.1. Связи между таблицами [#373615]

--- sql/ User rights and rollers scheme[#1093]/insert.sql
+++ sql/ User rights and rollers scheme[#1093]/insert.sql
@@ -6,11 +6,13 @@ select * from roles;
 insert into users(username, role_id) values ('Roman', 1);
 insert into users(username, role_id) values ('Jane', 3);
 insert into users(username, role_id) values ('Kass', 2);
+select * from users;
 
 insert into rules(rule) values ('Do everything you want');
 insert into rules(rule) values ('Do not touch anything');
 insert into rules(rule) values ('Can view');
 insert into rules(rule) values ('Can use');
+select * from rules;
 
 insert into roles_rules(role_id, rule_id) values (1, 1);
 insert into roles_rules(role_id, rule_id) values (1, 3);
@@ -26,9 +28,9 @@ insert into category(category) values ('Never');
 insert into state(state) values ('Where pc free');
 insert into state(state) values ('Where pc not free');
 
-insert into item(item, user_id, category_id, state_id) values ("Stay out", 3, 2, 2);
-insert into item(item, user_id, category_id, state_id) values ("Play game", 2, 1, 1);
-insert into item(item, user_id, category_id, state_id) values ("Learn Java", 1, 2, 1);
+insert into item(item, user_id, category_id, state_id) values ('Stay out', 3, 2, 2);
+insert into item(item, user_id, category_id, state_id) values ('Play game', 2, 1, 1);
+insert into item(item, user_id, category_id, state_id) values ('Learn Java', 1, 2, 1);
 
 insert into comments(comment, item_id) values ('Funny game', 2);
 insert into comments(comment, item_id) values ('Do not touch my pc', 1);
@@ -37,15 +39,3 @@ insert into comments(comment, item_id) values ('Like a senior', 3);
 insert into attachs(attach, item_id) values ('Idea', 1);
 insert into attachs(attach, item_id) values ('Dota 2', 2);
 insert into attachs(attach, item_id) values ('Power switch', 3);
-
-
-
-/*select * from users;
-select * from roles;
-select * from rules;
-select * from roles_rules;
-select * from category;
-select * from state;
-select * from item;
-select * from comments;
-select * from attachs;*/

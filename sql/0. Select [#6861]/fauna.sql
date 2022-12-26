c2566b2f49b62d9392fe15163c6a7459010708c7
Roman <roman_stalmakov@mail.ru>
2022-12-26 17:36:58 +0300

    0. Select [#6861]

--- sql/0. Select [#6861]/fauna.sql
+++ sql/0. Select [#6861]/fauna.sql
@@ -6,22 +6,22 @@ create table fauna (
 );
 
 insert into fauna(animal_name, avg_age, discovery_date) 
-    values ('huso huso - fish', 100, '01.01.1758');
+    values ('huso huso - fish', 100, '1758-01-01');
 insert into fauna(animal_name, avg_age, discovery_date) 
-    values ('Eunectes murinus - snake', 20, '01.01.1758');
+    values ('Eunectes murinus - snake', 20, '1758-01-01');
 insert into fauna(animal_name, avg_age, discovery_date) 
     values ('Cubozoa - medusa', 1, null);
 insert into fauna(animal_name, avg_age, discovery_date) 
-    values ('Delphinus delphis - dolphin', 25, '01.01.1758');
+    values ('Delphinus delphis - dolphin', 25, '1758-01-01');
 insert into fauna(animal_name, avg_age, discovery_date) 
-    values ('Serrasalmus rhombeus - dolphin', 4, '01.01.1766');
+    values ('Serrasalmus rhombeus - dolphin', 4, '1766-01-01');
 insert into fauna(animal_name, avg_age, discovery_date) 
-    values ('Panthera tigris- tiger', 18, '01.01.1758');
+    values ('Panthera tigris- tiger', 18, '1758-01-01');
 
 select * from fauna where animal_name like '%fish%';
 
-select * from fauna where avg_age >=10 AND avg_age <= 21;
+select * from fauna where avg_age >= 10 AND avg_age <= 21;
 
 select * from fauna where discovery_date is null;
 
-select * from fauna where discovery_date < '01.01.1950';
+select * from fauna where discovery_date < '1950-01-01';

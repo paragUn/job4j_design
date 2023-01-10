121b3cb20a72719c076f02a3e9236ff7bd0383cb
Roman <roman_stalmakov@mail.ru>
2023-01-10 14:11:55 +0300

    0. Join [#6862]

--- sql/0. Join [#6862]/0. Join [#6862].sql
+++ sql/0. Join [#6862]/0. Join [#6862].sql
@@ -38,3 +38,71 @@ select * from devices d cross join owners o;
 
 
 
+
+create table departments(
+    id serial primary key,
+    name varchar(30)
+);
+
+create table employees(
+    id serial primary key,
+    name varchar(30),
+    department_id int references departments(id)
+);
+
+insert into departments (name) values ('department 1');
+insert into departments (name) values ('department 2');
+insert into departments (name) values ('department 3');
+
+insert into employees (name, department_id) values ('employee 1', 1);
+insert into employees (name, department_id) values ('employee 2', 1);
+insert into employees (name, department_id) values ('employee 3', 3);
+insert into employees (name, department_id) values ('employee 4', 1);
+insert into employees (name, department_id) values ('employee 5', null);
+
+
+select * from departments;
+select * from employees;
+
+--left
+select * from departments d left join employees e on e.department_id = d.id;
+
+--right
+select * from departments d right join employees e on e.department_id = d.id;
+
+--full
+select * from departments d full join employees e on e.department_id = d.id;
+
+--cross
+select * from departments d cross join employees e;
+
+--null departments
+select * from departments d left join employees e on e.department_id = d.id
+where e.department_id is null ;
+
+--left same
+select * from departments d left join employees e on d.id = e.department_id
+where e.department_id = 1;
+
+--right same
+select * from departments d right join employees e on d.id = e.department_id
+where d.id = 1;
+
+CREATE TABLE teens (
+ 	id serial primary key,
+	name varchar(30),
+	gender varchar(20)
+);
+INSERT INTO teens(name,gender) values ('Roman','male');
+INSERT INTO teens(name,gender) values ('Romeo','male');
+INSERT INTO teens(name,gender) values ('Khristina','female');
+INSERT INTO teens(name,gender) values ('Jane','female');
+INSERT INTO teens(name,gender) values ('Anastasia','female');
+
+SELECT * FROM teens;
+
+select t1.name as boy, t2.name as girl from teens t1 cross join teens t2
+where t1.gender != t2.gender;
+
+
+

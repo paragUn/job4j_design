98000e75ef63e8028c3103a59d7d09406c656c3e
Roman <roman_stalmakov@mail.ru>
2023-01-25 12:06:37 +0300

    5. Курсор в SQL [#504816]

---
+++ sql/5. Cursor in SQL [#504816]/Cursor.sql
@@ -0,0 +1,76 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    count integer default 0,
+    price integer
+);
+
+insert into products (name, count, price) VALUES ('product_1', 1, 5);
+insert into products (name, count, price) VALUES ('product_2', 2, 10);
+insert into products (name, count, price) VALUES ('product_3', 3, 15);
+insert into products (name, count, price) VALUES ('product_4', 4, 20);
+insert into products (name, count, price) VALUES ('product_5', 5, 25);
+insert into products (name, count, price) VALUES ('product_6', 6, 30);
+insert into products (name, count, price) VALUES ('product_7', 7, 35);
+insert into products (name, count, price) VALUES ('product_8', 8, 40);
+insert into products (name, count, price) VALUES ('product_9', 9, 45);
+insert into products (name, count, price) VALUES ('product_10', 10, 50);
+insert into products (name, count, price) VALUES ('product_11', 11, 55);
+insert into products (name, count, price) VALUES ('product_12', 12, 60);
+insert into products (name, count, price) VALUES ('product_13', 13, 65);
+insert into products (name, count, price) VALUES ('product_14', 14, 70);
+insert into products (name, count, price) VALUES ('product_15', 15, 75);
+insert into products (name, count, price) VALUES ('product_16', 16, 80);
+insert into products (name, count, price) VALUES ('product_17', 17, 85);
+insert into products (name, count, price) VALUES ('product_18', 18, 90);
+insert into products (name, count, price) VALUES ('product_19', 19, 95);
+insert into products (name, count, price) VALUES ('product_20', 20, 100);
+
+select * from products;
+
+begin transaction;
+
+DECLARE cursor_p cursor for select * from products;
+
+fetch 10 from cursor_p;
+
+fetch from cursor_p;
+
+fetch from cursor_p;
+
+move forward 2 from cursor_p;
+
+fetch next from cursor_p;
+
+fetch 5 from cursor_p;
+fetch prior from cursor_p;
+
+move backward 4 from cursor_p;
+
+move backward 1 from cursor_p;
+
+fetch from cursor_p;
+
+move prior from cursor_p;
+fetch from cursor_p;
+fetch from cursor_p;
+
+move backward 3 from cursor_p;
+
+fetch from cursor_p;
+
+move backward 9 from cursor_p;
+
+fetch from cursor_p;
+
+move backward 6 from cursor_p;
+
+fetch from cursor_p;
+
+move backward 2 from cursor_products;
+
+fetch from cursor_products;
+
+close cursor_products;
+
+commit;

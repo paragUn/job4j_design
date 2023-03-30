6a44070cd92c5cedd505273c4b9d59f385148240
Roman <roman_stalmakov@mail.ru>
2023-03-30 16:14:58 +0300

    4. Найти утечку памяти. [#504882]

--- src/main/java/ru/job4j/gc/leak/UserGenerator.java
+++ src/main/java/ru/job4j/gc/leak/UserGenerator.java
@@ -10,14 +10,12 @@ public class UserGenerator implements Generate {
     public static final String PATH_NAMES = "src/main/java/ru/job4j/gc/leak/files/names.txt";
     public static final String PATH_SURNAMES = "src/main/java/ru/job4j/gc/leak/files/surnames.txt";
     public static final String PATH_PATRONS = "src/main/java/ru/job4j/gc/leak/files/patr.txt";
+    public int count = 50;
 
-    public static final String SEPARATOR = " ";
-    public static final Integer NEW_USERS = 1000;
-
-    public static List<String> names;
-    public static List<String> surnames;
-    public static List<String> patrons;
-    private static List<User> users = new ArrayList<>();
+    public List<String> names;
+    public List<String> surnames;
+    public List<String> patrons;
+    private List<User> users = new ArrayList<>();
     private Random random;
 
     public UserGenerator(Random random) {
@@ -28,11 +26,11 @@ public class UserGenerator implements Generate {
     @Override
     public void generate() {
         users.clear();
-        for (int i = 0; i < NEW_USERS; i++) {
-            users.add(new User(
-                    surnames.get(random.nextInt(surnames.size())) + SEPARATOR
-                            + names.get(random.nextInt(names.size())) + SEPARATOR
-                            + patrons.get(random.nextInt(patrons.size()))));
+        for (int i = 0; i < count; i++) {
+            users.add(new User(String.join(
+                    surnames.get(random.nextInt(surnames.size())),
+                            names.get(random.nextInt(names.size())),
+                            patrons.get(random.nextInt(patrons.size())))));
         }
     }
 
@@ -50,7 +48,7 @@ public class UserGenerator implements Generate {
         return users.get(random.nextInt(users.size()));
     }
 
-    public static List<User> getUsers() {
+    public List<User> getUsers() {
         return users;
     }
 }
